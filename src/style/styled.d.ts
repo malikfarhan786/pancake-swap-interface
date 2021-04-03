83c489aa1841760102c7a838ee524ecad320d5e8
malikfarhan786 <69869680+malikfarhan786@users.noreply.github.com>
2021-04-04 01:22:37 +0500

    Update styled.d.ts

--- src/style/styled.d.ts
+++ src/style/styled.d.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-unresolved
-import { PancakeTheme } from 'pushswap-librarytest/dist/theme'
+import { PancakeTheme } from 'pushswap-librarytest2/dist/theme'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
